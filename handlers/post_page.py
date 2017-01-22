6ced80add1544dfb0b71716892dc2b7f57691efa
ihor1337 <ihor1337@gmail.com>
2017-01-21 21:06:50 -0800

    removed redudunt check if post exists in post_page handler

--- handlers/post_page.py
+++ handlers/post_page.py
@@ -15,9 +15,6 @@ class PostPage(BlogHandler):
         p = BlogParent.get_by_key_name('key')
         likes = db.GqlQuery("SELECT * FROM Votes WHERE ANCESTOR IS :1 AND post = :2 ORDER BY rating DESC",
                             p, post).get()
-        if not post:
-            self.error(404)
-            return
         if self.get_user():
             token = self.generate_csrf_token(self.user).split('.')[1]
             self.render('permalink.html', post=post, comment=c, likes=likes.rating, token=token)
